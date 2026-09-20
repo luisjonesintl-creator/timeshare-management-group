b1ad83b9c5748ed1cd017c8f279db44c66162758
luisjonesintl-creator <luisjonesintl@gmail.com>
2026-09-20 19:42:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // Enlace del servidor de datos dedicado corregido
 const SUPABASE_URL = "https://ztojbyfbyidzzrqicjvn.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0b2pieWZieWlkenpycWljanZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5MzE1NTUsImV4cCI6MjEwNTUwNzU1NX0.Ap30zRwDs3z3vBjyS2ibocx5oGY3Uzft2eID26RbLKQ";
-
 let supabaseClientInstance = null;
 
 // ====== INITIALIZATION ROUTINE ======
