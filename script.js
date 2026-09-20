ebdf5527e03cad279e5783f4b3133e36de47227d
luisjonesintl-creator <luisjonesintl@gmail.com>
2026-09-20 19:07:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // ====== CONFIGURATION STEP ======
-// ====== CONFIGURATION STEP ======
-// FIX: Changed from general homepage link to your unique project URL database link
 const SUPABASE_URL = "https://ztobjbyfbyidzzrqicjvn.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0b2pieWZieWlkenpycWljanVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5MzE1NTUsImV4cCI6MjEwNTUwNzU1NX0.Ap30zRwDs3z3vBjyS2ibocx5oGY3Uzft2eID26RbLKQ";
-
 
 let supabaseClientInstance = null;
 
